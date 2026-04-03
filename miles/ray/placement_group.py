c5aed62d17fd864865e2b1eef8e2191726c17cdb
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2026-04-03 14:16:42 +0800

    refactor async_init API to allow re-create later (#839)

--- miles/ray/placement_group.py
+++ miles/ray/placement_group.py
@@ -120,13 +120,15 @@ def create_placement_groups(args):
     }
 
 
-def allocate_train_group(args, num_nodes, num_gpus_per_node, pg):
+def allocate_train_group(args, num_nodes, num_gpus_per_node, pg, role: str, with_ref: bool):
     return RayTrainGroup(
         args=args,
         num_nodes=num_nodes,
         num_gpus_per_node=num_gpus_per_node,
         pg=pg,
         num_gpus_per_actor=0.4,
+        role=role,
+        with_ref=with_ref,
     )
 
 
@@ -136,6 +138,8 @@ def create_training_models(args, pgs, rollout_manager):
         num_nodes=args.actor_num_nodes,
         num_gpus_per_node=args.actor_num_gpus_per_node,
         pg=pgs["actor"],
+        role="actor",
+        with_ref=args.kl_coef != 0 or args.use_kl_loss,
     )
     if args.use_critic:
         critic_model = allocate_train_group(
@@ -143,14 +147,14 @@ def create_training_models(args, pgs, rollout_manager):
             num_nodes=args.critic_num_nodes,
             num_gpus_per_node=args.critic_num_gpus_per_node,
             pg=pgs["critic"],
+            role="critic",
+            with_ref=False,
         )
-        critic_init_handle = critic_model.async_init(args, role="critic", with_ref=False)
+        critic_init_handle = critic_model.async_init()
     else:
         critic_model = None
 
-    start_rollout_ids = ray.get(
-        actor_model.async_init(args, role="actor", with_ref=args.kl_coef != 0 or args.use_kl_loss)
-    )
+    start_rollout_ids = ray.get(actor_model.async_init())
 
     assert len(set(start_rollout_ids)) == 1
     if args.start_rollout_id is None:
